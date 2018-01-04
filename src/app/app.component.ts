ff090980f7eafdca2a86222333414e20bf6fd003
Jay Lin <jay101630@gmail.com>
2018-01-04 21:15:42 +0800

    current conditions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,83 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { WeatherService } from './services/weather.service';
+import { Weather } from './models/weather';
+import { Console } from '@angular/core/src/console';
+import { MouseEvent } from '@agm/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Weather app';
-  lat: number = 23.5;
-  lng: number = 121.0;
+  mapLat: number = 23.5;
+  mapLng: number = 121.0;
+
+  citys: any[] = [
+    {//taipei
+      cityName:"Taipei",
+      temp_c: "N/A",
+      wind_kph: "N/A",
+      lat: 25.06999969,
+      lng: 121.55000305
+    },
+    {//hsin-chu
+      cityName:"Hsin-chu",
+      temp_c: "N/A",
+      wind_kph: "N/A",
+      lat: 24.79999924,
+      lng: 120.97000122
+    }
+  ];
+
+  constructor(private weatherService: WeatherService) {}
+
+  ngOnInit() {
+    this.getCurreentConditions("Taipei");
+    this.getCurreentConditions("Hsin-chu");
+  }
+
+  getCurreentConditions(location: string)
+  {
+    this.weatherService.getCurreentConditions(location).subscribe(
+      data => {
+        for(var i = 0; i < this.citys.length; i++)
+        {
+
+          if(this.citys[i].cityName == location)
+          {
+            console.log(this.citys[i].cityName);
+            this.citys[i].temp_c = data['current_observation']['temp_c'];
+            this.citys[i].wind_kph = data['current_observation']['wind_kph'];
+          }
+        }
+      }
+    )
+  }
+
+  mapClicked($event: MouseEvent) {
+    this.weatherService.getGeoConditions($event.coords.lat, $event.coords.lng).subscribe(
+      data => {
+        console.log(`/geolookup/conditions/q/${$event.coords.lat},${$event.coords.lng}.json`);
+        var temp_c ="N/A";
+        var wind_kph ="N/A";
+
+        if(data.hasOwnProperty('current_observation'))
+        {
+          temp_c = data['current_observation']['temp_c'];
+          wind_kph = data['current_observation']['wind_kph'];
+        }
+        var cityName = data['location']['city'];
+        this.citys.push({
+          cityName: cityName,
+          temp_c: temp_c,
+          wind_kph: wind_kph,
+          lat: $event.coords.lat,
+          lng: $event.coords.lng
+        });
+      }
+    );
+  }
 }
---
+++ src/app/models/weather.ts
@@ -0,0 +1,6 @@
+export interface Weather {
+  cityName: string;
+  temp_c: number;
+  lat: number;
+  lng: number;
+}
